1c919a14bb5f371c8967da34aa9893009fd7d17e
Elliott Phillips <elliottphillips@Elliotts-MacBook-Air.local>
2024-05-26 19:17:36 -0400

    Initial Commit

---
+++ token/token.go
@@ -0,0 +1,66 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+const (
+	// Special Types
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifies & Literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// OPERATORS
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	// Equalities
+	EQ     = "=="
+	NOT_EQ = "!="
+)
